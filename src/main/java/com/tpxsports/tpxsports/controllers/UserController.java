5347c58c39a33745e395f979e97ba8635548e8b4
Raimundo Junior <ychainstyle@gmail.com>
2022-04-23 18:25:23 -0300

    Adicionando cálculo de kd e validação de usuários notFound.

--- src/main/java/com/tpxsports/tpxsports/controllers/UserController.java
+++ src/main/java/com/tpxsports/tpxsports/controllers/UserController.java
@@ -4,10 +4,12 @@ import com.tpxsports.tpxsports.domains.User;
 import com.tpxsports.tpxsports.repository.UserRepository;
 import com.tpxsports.tpxsports.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/users")
@@ -23,9 +25,13 @@ public class UserController {
     }
 
     @GetMapping(path = "/stats/{id}")
-    public @ResponseBody
-    User getAllUsers(@PathVariable Integer id) {
-        return service.findStatsUser(id);
+    public ResponseEntity<User> getStatsUser(@PathVariable Integer id) {
+
+        return Optional
+                .ofNullable(service.findStatsUser(id))
+                .map(user -> ResponseEntity.ok().body(user))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+
     }
 
 }
--- src/main/java/com/tpxsports/tpxsports/services/UserService.java
+++ src/main/java/com/tpxsports/tpxsports/services/UserService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +15,26 @@ public class UserService {
     private final UserRepository userRepository;
 
     public List<User> findAllUsers() {
-        return userRepository.findAll();
+        List<User> listUser = userRepository.findAll();
+        listUser.forEach(user -> {
+            calcKD(user);
+        });
+        return listUser;
     }
 
     public User findStatsUser(Integer id) {
-        return userRepository.findById(id).orElse(null);
+        var user = userRepository.findById(id).orElse(null);
+
+        calcKD(user);
+
+        return user;
+    }
+
+    private void calcKD(User user) {
+        if (Objects.nonNull(user)) {
+            Double kd = (double) user.getKills() / (double) user.getDeaths();
+            user.setKd(kd);
+        }
     }
 
 }
