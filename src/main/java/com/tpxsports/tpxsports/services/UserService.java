ae5640e19956438eeabb37a03cac3baa639bbe41
julliocesar03 <jullio_gap@hotmail.com>
2022-05-18 23:08:55 -0300

    Implementação com o truncamento para o valor de Kd, e a troca do tipo da variável para BigDecimal

--- src/main/java/com/tpxsports/tpxsports/services/UserService.java
+++ src/main/java/com/tpxsports/tpxsports/services/UserService.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.Objects;
 
@@ -32,8 +34,15 @@ public class UserService {
 
     private void calcKD(User user) {
         if (Objects.nonNull(user)) {
-            Double kd = (double) user.getKills() / (double) user.getDeaths();
-            user.setKd(kd);
+
+            BigDecimal bdk = BigDecimal.valueOf(user.getKills());
+            BigDecimal bdd = BigDecimal.valueOf(user.getDeaths());
+
+            if (bdd.compareTo(BigDecimal.ZERO) == 0){
+                user.setKd(bdk);
+            } else {
+                user.setKd(bdk.divide(bdd, 2, RoundingMode.HALF_EVEN));
+            }
         }
     }
 
